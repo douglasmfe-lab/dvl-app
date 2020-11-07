f989364eb97f93733631bc7129872e68f97ed849
Douglas Ferreira <douglasmfe@gmail.com>
2020-11-07 16:25:21 -0300

    Item 1 - Ordenação Skill por nome

--- src/main/java/com/dvlcube/app/rest/SkillService.java
+++ src/main/java/com/dvlcube/app/rest/SkillService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,7 +40,7 @@ public class SkillService implements MxFilterableBeanService<SkillBean, Long> {
 	@Override
 	@GetMapping
 	public Iterable<SkillBean> get(@RequestParam Map<String, String> params) {
-		return repo.firstPage();
+		return repo.firstPage(Sort.by("name"));
 	}
 
 	@Override
