9e7479aff57e179e74f3cf7a722a197568c3d6a7
Douglas Ferreira <douglasmfe@gmail.com>
2020-11-08 16:11:23 -0300

    Criacao de rotas para o JobBean

--- src/main/java/com/dvlcube/app/manager/data/JobBean.java
+++ src/main/java/com/dvlcube/app/manager/data/JobBean.java
@@ -1,6 +1,8 @@
 package com.dvlcube.app.manager.data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.dvlcube.utils.interfaces.MxBean;
@@ -10,11 +12,12 @@ import com.dvlcube.utils.interfaces.Nameable;
  * @since 3 de jun de 2019
  * @author Ulisses Lima
  */
-@Entity
+@Entity(name = "job_bean")
 public class JobBean implements MxBean<Long>, Nameable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private Integer max;
---
+++ src/main/java/com/dvlcube/app/rest/JobService.java
@@ -0,0 +1,62 @@
+package com.dvlcube.app.rest;
+
+import static com.dvlcube.app.manager.data.e.Menu.CONFIGURATION;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dvlcube.app.interfaces.MenuItem;
+import com.dvlcube.app.jpa.repo.JobRepository;
+import com.dvlcube.app.manager.data.JobBean;
+import com.dvlcube.app.manager.data.vo.MxRestResponse;
+import com.dvlcube.utils.interfaces.rest.MxFilterableBeanService;
+
+@RestController
+@MenuItem(value = CONFIGURATION)
+@RequestMapping("${dvl.rest.prefix}/jobs")
+public class JobService implements MxFilterableBeanService<JobBean, Long> {
+
+	@Autowired
+	private JobRepository repo;
+	
+	@Override
+	@GetMapping
+	public Iterable<JobBean> get(@RequestParam Map<String, String> params) {
+		return repo.firstPage();
+	}
+
+	
+	@Override
+	@GetMapping("/{id}")
+	public Optional<JobBean> get(@PathVariable Long id) {
+		return repo.findById(id);
+	}
+	
+	@Override
+	@PostMapping
+	public MxRestResponse post(@RequestBody JobBean body) {
+		return GenericRestResponse.ok(repo.save(body).getId());
+	}
+
+	@Override
+	public Iterable<JobBean> getLike(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repo.deleteById(id);
+	}
+
+}
